b407fcc05ccb8fdfa8250dd9040e4b589d966170
0Armaan025 <armaan33000@gmail.com>
2023-08-12 20:05:12 +0530

    feat: made query screen

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import '../features/auth/screens/login/screens/login_screen.dart';
+import '../features/eco_shopping_guru/screens/bag_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,12 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DashboardScreen(
-          distanceTravelled: 2,
-          ecoCoins: 2,
-          foodSaved: 2,
-          resourcesWasted: 2,
-          totalCarbonFootprints: 2),
+      home: BagScreen(),
     );
   }
 }
